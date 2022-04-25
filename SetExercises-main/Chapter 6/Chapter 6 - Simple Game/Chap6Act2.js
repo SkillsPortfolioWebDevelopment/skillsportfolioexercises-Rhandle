e6b120ff3328c0b010931f75c9ed934e6a966daa
Rhandle <91404648+Rhandle@users.noreply.github.com>
2022-04-25 12:11:31 +0400

    Add files via upload

---
+++ SetExercises-main/Chapter 6/Chapter 6 - Simple Game/Chap6Act2.js
@@ -0,0 +1,28 @@
+﻿var score = 0;
+var time = 6000;
+
+function moveShape(move) {
+    //Randomizes location of html element
+    move.style.position = 'relative';
+    move.style.top = Math.floor(Math.random() * 450) + 'px';
+    move.style.left = Math.floor(Math.random() * 450) + 'px';
+};
+var block = document.querySelector('.block');
+block.addEventListener('click', function (e) {
+    //Moves html element 'block' after clicking
+    moveShape(e.target);
+    score++
+    document.getElementById("score").innerHTML = Math.floor(score);
+});
+var timer = setInterval(function () {
+    if (time != 0) {
+        time--;
+        document.getElementById("timer").innerHTML = Math.floor(time / 200);
+    }
+    else {
+        time = 0;
+        alert("Game Over. Score: " + score);
+    }
+});
+
+
---
+++ SetExercises-main/Chapter 6/Chapter 6 - Temperature Converter/Chap6Act1.js
@@ -0,0 +1,11 @@
+﻿const btn = document.createElement('button');
+btn.classList.add('btn');
+btn.innerText = "Convert";
+
+btn.addEventListener('click', () => {
+    let valNum = document.getElementById("tempconverter");
+    document.getElementById("celsius").innerHTML = (parseFloat(valNum.value) - 32) * 5 / 9;
+    document.getElementById("kelvin").innerHTML = (parseFloat(valNum.value) - 32) * 5 / 9 + 273.15;
+});
+
+document.getElementById('buttons').appendChild(btn);

f554386755f3b06229324384f7e8656367d0e867
Rhandle <91404648+Rhandle@users.noreply.github.com>
2022-04-25 15:48:35 +0400

    Add files via upload

---
+++ SetExercises-main/Chapter 5/Chapter 5 - Alan Partridge Soundboard/Chap5Act2.js
@@ -0,0 +1,34 @@
+﻿const sounds = [
+    'Ahha',
+    'Backofthenet',
+    'Bangoutoforder',
+    'Dan',
+    'Goal',
+    'Hellopartridge',
+    'Jurassicpark',
+    'Kissmyface',
+    'Smellmycheese'
+];
+
+sounds.forEach((sound) => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+    btn.innerText = sound;
+
+    function stopSongs() { // To keep the sounds from overlapping
+        sounds.forEach((sound) => {
+            const song = document.getElementById(sound);
+
+            song.pause();
+            song.currentTime = 0; 
+        });
+    };
+
+    btn.addEventListener('click', () => {
+        stopSongs();
+        document.getElementById(sound).play();
+    });
+
+    document.getElementById('buttons').appendChild(btn);
+
+});
